09568792444c701121232fbb6339badefcbfb42d
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-28 08:08:37 +0300

    Update RedeemCodeCommand.php

--- src/mfdgaming/voucheradvanced/command/RedeemCodeCommand.php
+++ src/mfdgaming/voucheradvanced/command/RedeemCodeCommand.php
@@ -28,7 +28,8 @@ class RedeemCodeCommand extends PluginCommand {
 					$codes = array_values($codes);
 					$this->plugin->codes->set("codes", $codes);
 					$this->plugin->codes->save();
-					$this->plugin->economy->addMoney($sender, 100);
+					$money = $this->plugin->getConfig()->get("money-to-gain");
+					$this->plugin->economy->addMoney($sender, $money);
 					$sender->sendMessage(TextFormat::GREEN . "Successfully redeemed the redeemcode " . $code);
 					return true;
 				} else {

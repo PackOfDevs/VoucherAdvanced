3c7a8cf6c33ca254d8a77e940e57ea5dfaa4af7c
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-28 06:46:47 +0300

    Update VoucherAdvanced.php

--- mfdgaming/voucheradvanced/VoucherAdvanced.php
+++ mfdgaming/voucheradvanced/VoucherAdvanced.php
@@ -13,5 +13,13 @@ class VoucherAdvanced extends PluginBase {
 
 	public function onEnable() {
 		$this->getLogger()->info($this->prefix . "Enabled!");
+		$this->registerCommands();
+	}
+	
+	public function registerCommands() {
+		$commands = [new GenerateCodeCommand($this), new RedeemCommand($this)];
+		foreach($commands as $command) {
+			$this->getServer()->getCommandMap()->register("command", $command);
+		}
 	}
 }

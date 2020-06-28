aae088abc064e66d21ca94ae29153c4a5074d16c
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-28 06:39:52 +0300

    Update VoucherAdvanced.php

--- mfdgaming/voucheradvanced/VoucherAdvanced.php
+++ mfdgaming/voucheradvanced/VoucherAdvanced.php
@@ -5,8 +5,9 @@ namespace mfdgaming\voucheradvanced;
 use pocketmine\plugin\PluginBase;
 
 class VoucherAdvanced extends PluginBase {
+	public $prefix = "> ";
+
 	public function onEnable() {
-
-
-
+		$this->getLogger()->info($this->prefix . "Enabled!");
+	}
 }

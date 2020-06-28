16d8072950107f86d59dafec2402fe57252ec285
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-28 07:01:28 +0300

    Update GenerateCodeCommand.php

--- mfdgaming/voucheradvanced/command/GenerateCodeCommand.php
+++ mfdgaming/voucheradvanced/command/GenerateCodeCommand.php
@@ -9,6 +9,18 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 
-class GenerateCodeCommand {
-
+class GenerateCodeCommand extends PluginCommand {
+	public function __construct(Main $owner) {
+		parent::__construct("generatecode", $owner);
+		$this->setDescription("Generate Code Command");
+		$this->setAliases(["gencode"]);
+		$this->setPermission("use.voucheradvanced.generatecode");
+		$this->plugin = $owner;
+	}
+	public function execute(CommandSender $sender, $currentAlias, array $args) {
+		if(!$this->testPermission($sender)){
+			$sender->sendMessage(TextFormat::RED . "You dont have the permition to execute this command");
+			return true;
+		}
+	}
 }

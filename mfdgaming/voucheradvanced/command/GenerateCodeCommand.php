f211bb59ea43468c1b58955af93fa8f508ab6121
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-28 06:56:56 +0300

    Create GenerateCodeCommand.php

---
+++ mfdgaming/voucheradvanced/command/GenerateCodeCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace mfdgaming\voucheradvanced\command;
+
+use mfdgaming\voucheradvanced\VoucherAdvanced as Main;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class GenerateCodeCommand {
+
+}

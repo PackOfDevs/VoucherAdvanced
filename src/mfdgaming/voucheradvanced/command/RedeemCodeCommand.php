6f1fde64ae6d3b2f8b441257eece8b923384ba2f
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-30 14:34:09 +0300

    Update RedeemCodeCommand.php

--- src/mfdgaming/voucheradvanced/command/RedeemCodeCommand.php
+++ src/mfdgaming/voucheradvanced/command/RedeemCodeCommand.php
@@ -4,6 +4,7 @@ namespace mfdgaming\voucheradvanced\command;
 
 use mfdgaming\voucheradvanced\VoucherAdvanced as Main;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\Player;
 use pocketmine\Server;
